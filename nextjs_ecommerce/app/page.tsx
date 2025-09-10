2b4a29763bc668cce9d080c5e42e0eb332de3b61
Moetaz <motaz062002@gmail.com>
2025-09-10 18:42:00 +0100

    home section Category section& cardSection

--- nextjs_ecommerce/app/page.tsx
+++ nextjs_ecommerce/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Hero from '../Components/home/Hero'
+import CategorySection from '../Components/home/CategorySections'
 const HomePage = () => {
   return (
     <div>
-      hello world ss
+      <Hero/>
+      <CategorySection/>
     </div>
   )
 }
