c605883bc526ee8cf15fbf9786524193db4c4def
Moetaz <motaz062002@gmail.com>
2025-09-10 18:12:02 +0100

    NavBar

--- nextjs_ecommerce/Components/NavBar/NavBar.tsx
+++ nextjs_ecommerce/Components/NavBar/NavBar.tsx
@@ -1,7 +1,18 @@
+"use client"
+
 import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import SearchForm from './SearchForm'
+import SearchButton from './SearchButton'
+import MobileNavbar from './MobileNavbar'
+import NavItems from './NavItems'
 const NavBar = () => {
+  const [showSearchForm, setShowSearchForm] = useState(false);
+
+    const handleSearch = () => {
+    setShowSearchForm((curr) => !curr);
+  };
   return (
         <>
     <nav className="bg-[whitesmoke] sticky top-0 z-20 w-full py-4">
@@ -15,12 +26,16 @@ const NavBar = () => {
           </div>
 
           <div className="max-lg:block hidden">
+            <SearchButton handleSearch={handleSearch}
+              showSearchForm={showSearchForm} />
           </div>
 
           <div className="max-md:hidden">
+            <NavItems />
           </div>
 
           <div className="max-md:block hidden">
+            <MobileNavbar />
           </div>
         </div>
     </nav> 
---
+++ nextjs_ecommerce/Components/NavBar/NavBarContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const NavBarContainer = () => {
+  return (
+    <nav className="bg-[whitesmoke] sticky top-0 z-20 w-full py-4">
+        <NavBar />
+    </nav>
+  )
+}
+
+export default NavBarContainer
