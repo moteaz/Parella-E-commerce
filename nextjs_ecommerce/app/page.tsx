5c835aeef1ba99cc70572481b33a927b462ffd24
Moetaz <motaz062002@gmail.com>
2025-09-11 17:54:03 +0100

    Creating The Product Section

--- nextjs_ecommerce/app/page.tsx
+++ nextjs_ecommerce/app/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from '../Components/home/Hero'
 import CategorySection from '../Components/home/CategorySections'
+import ProductSection from '../Components/home/ProductSection'
 const HomePage = () => {
   return (
     <div>
       <Hero/>
       <CategorySection/>
+      <ProductSection title="Featured Products"/>
+      <ProductSection title="Best Selling Products"/>
+      <ProductSection title="New Arrivals"/>
     </div>
   )
 }
